267e261179aaebad543f8c0c33e6b673ac921b85
Mayel de Borniol <git@mayel.space>
2024-04-03 14:06:31 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/869

--- lib/integration.ex
+++ lib/integration.ex
@@ -5,6 +5,12 @@ defmodule Bonfire.Posts.Integration do
   # alias Bonfire.Data.Social.Follow
   import Untangle
 
+  declare_extension("Posts",
+    icon: "icomoon-free:blog",
+    emoji: "📝",
+    description: l("Functionality for writing and reading posts.")
+  )
+
   def repo, do: Config.repo()
 
   def mailer, do: Config.get!(:mailer_module)
