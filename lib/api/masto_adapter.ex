bec0c3bac9d8f0ecd95bcb6eb4ccbaa5d22577ce
ivan <ivanminutillo@outlook.it>
2026-05-26 14:04:21 +0200

    masto

--- lib/api/masto_adapter.ex
+++ lib/api/masto_adapter.ex
@@ -43,7 +43,7 @@ defmodule Bonfire.Posts.API.MastoAdapter do
   end
 
   defp publish_from_masto_params(params, current_user) do
-    with {:ok, post_attrs} <- build_post_attrs(params),
+    with {:ok, post_attrs} <- build_post_attrs(params, current_user),
          {boundary, extra_circles} <- visibility_to_boundary(params["visibility"]),
          opts <- build_publish_opts(params, current_user, boundary, extra_circles, post_attrs),
          {:ok, post} <- Bonfire.Posts.publish(opts) do
@@ -51,24 +51,69 @@ defmodule Bonfire.Posts.API.MastoAdapter do
     end
   end
 
-  defp build_post_attrs(params) do
-    media = fetch_media_by_ids(params["media_ids"] || params["media_ids[]"] || [])
+  defp build_post_attrs(params, current_user) do
     status_text = params["status"] || ""
 
-    if status_text == "" and media == [] do
-      {:error, "Validation failed: Text can't be blank"}
-    else
-      {:ok,
-       %{
-         post_content: %{
-           html_body: status_text,
-           summary: params["spoiler_text"]
-         },
-         reply_to_id: params["in_reply_to_id"],
-         uploaded_media: media
-       }}
+    with {:ok, media} <-
+           fetch_media_by_ids(params["media_ids"] || params["media_ids[]"] || [], current_user) do
+      if status_text == "" and media == [] do
+        {:error, "Validation failed: Text can't be blank"}
+      else
+        {:ok,
+         %{
+           post_content: %{
+             html_body: status_text,
+             summary: params["spoiler_text"]
+           },
+           reply_to_id: params["in_reply_to_id"],
+           uploaded_media: media
+         }}
+      end
     end
   end
+
+  defp fetch_media_by_ids(nil, _current_user), do: {:ok, []}
+  defp fetch_media_by_ids([], _current_user), do: {:ok, []}
+
+  defp fetch_media_by_ids(media_ids, current_user) when is_list(media_ids) do
+    media_ids
+    |> Enum.reduce_while({:ok, []}, fn media_id, {:ok, media} ->
+      case fetch_owned_media(media_id, current_user) do
+        {:ok, item} -> {:cont, {:ok, [item | media]}}
+        {:error, reason} -> {:halt, {:error, reason}}
+      end
+    end)
+    |> case do
+      {:ok, media} -> {:ok, Enum.reverse(media)}
+      error -> error
+    end
+  end
+
+  defp fetch_media_by_ids(media_id, current_user) when is_binary(media_id) do
+    fetch_media_by_ids([media_id], current_user)
+  end
+
+  defp fetch_media_by_ids(media_ids, current_user) when is_map(media_ids) do
+    media_ids
+    |> Enum.sort_by(fn {index, _id} -> index end)
+    |> Enum.map(fn {_index, media_id} -> media_id end)
+    |> fetch_media_by_ids(current_user)
+  end
+
+  defp fetch_media_by_ids(_other, _current_user), do: {:ok, []}
+
+  defp fetch_owned_media(media_id, current_user) when is_binary(media_id) and media_id != "" do
+    current_user_id = id(current_user)
+
+    with {:ok, media} <- Bonfire.Files.Media.one(id: media_id),
+         true <- media.creator_id == current_user_id do
+      {:ok, media}
+    else
+      _ -> {:error, :not_found}
+    end
+  end
+
+  defp fetch_owned_media(_media_id, _current_user), do: {:error, :not_found}
 
   defp build_publish_opts(params, current_user, boundary, extra_circles, post_attrs) do
     [
@@ -85,30 +130,6 @@ defmodule Bonfire.Posts.API.MastoAdapter do
   defp visibility_to_boundary("private"), do: {"mentions", [{:followers, nil}]}
   defp visibility_to_boundary("direct"), do: {"mentions", []}
   defp visibility_to_boundary(_), do: {"public", []}
-
-  defp fetch_media_by_ids(nil), do: []
-  defp fetch_media_by_ids([]), do: []
-
-  defp fetch_media_by_ids(media_ids) when is_list(media_ids) do
-    media_ids
-    |> Enum.map(fn id ->
-      case Bonfire.Files.Media.one(id: id) do
-        {:ok, media} -> media
-        _ -> nil
-      end
-    end)
-    |> Enum.reject(&is_nil/1)
-  end
-
-  defp fetch_media_by_ids(media_id) when is_binary(media_id) do
-    fetch_media_by_ids([media_id])
-  end
-
-  defp fetch_media_by_ids(media_ids) when is_map(media_ids) do
-    fetch_media_by_ids(Map.values(media_ids))
-  end
-
-  defp fetch_media_by_ids(_other), do: []
 
   defp maybe_add_sensitive(opts, sensitive) when sensitive in [true, "true", "1"] do
     Keyword.put(opts, :sensitive, true)
